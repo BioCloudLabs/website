25c299da328c8a0e4d2d442909888015c439a44a
YourName <aymaneelhanbali@gmail.com>
2024-04-05 17:48:34 +0200

    On logout, Removing token works correctly

--- client/src/components/LogoutButton.tsx
+++ client/src/components/LogoutButton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const LogoutButton: React.FC<{ onLogout: () => void }> = ({ onLogout }) => {
   const handleLogout = () => {
     // Perform logout actions here, e.g., clearing the authentication token
-    localStorage.removeItem('authToken'); // Example: remove a token from local storage
+    localStorage.removeItem('token'); // Example: remove a token from local storage
 
     // Call the onLogout passed down from the parent component to update the isAuthenticated state
     onLogout();
@@ -13,5 +13,5 @@ const LogoutButton: React.FC<{ onLogout: () => void }> = ({ onLogout }) => {
 
   return( <button onClick={handleLogout}>Logout</button>);
 };
-    
+
 export default LogoutButton;

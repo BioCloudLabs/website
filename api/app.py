d76d3f0b7e0f4c72d75d1212a68d6b1cb3bb7c46
YourName <aymaneelhanbali@gmail.com>
2024-04-08 17:02:05 +0200

    Updated syntax to not use TEST at app.py config

--- api/app.py
+++ api/app.py
@@ -17,7 +17,7 @@ load_dotenv()
 app.config["API_TITLE"] = os.getenv("API_TITLE")
 app.config["API_VERSION"] = os.getenv("API_VERSION")
 app.config["OPENAPI_VERSION"] = os.getenv("OPENAPI_VERSION")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("TEST_SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
 jwt = JWTManager(app)

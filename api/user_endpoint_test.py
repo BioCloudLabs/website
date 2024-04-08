cde632ec39948a67bb387e78a209c24fd6250562
Christian González <chrisgonzaco@gmail.com>
2024-04-08 17:16:26 +0200

    profile tests finished

--- api/user_endpoint_test.py
+++ api/user_endpoint_test.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_smorest import Api
 import models
 import os 
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token
+from datetime import timedelta
 from database import db
 from flask_migrate import Migrate
 from controllers.user import blp as UserBlueprint
@@ -34,67 +35,54 @@ def client():
     with app.test_client() as client:
         yield client
 
+# Fixture to clean the database of tests users
 @pytest.fixture(autouse=True)
 def cleanup_db():
     with app.app_context():
-        db.session.query(models.UserModel).delete()
-        db.session.commit()
+        user_to_delete = models.UserModel.query.filter_by(email="test@example.com").first()
+        if user_to_delete:
+            db.session.delete(user_to_delete)
+            db.session.commit()
 
-# Create a fixture to get an auth token, used to test
+# Fixture to create an expired token, used to test
 @pytest.fixture
-def auth_token(client):
-    payload = {
+def expired_token():
+    with app.app_context():
+        expiration_delta = timedelta(seconds=-1)
+        expired_token = create_access_token(identity=184, expires_delta=expiration_delta)
+        return expired_token
+
+# Fixture to get a payload
+@pytest.fixture
+def payload():
+    return {
         "email": "test@example.com",
         "password": "password",
         "name": "Test",
         "surname": "User",
         "location_id": 1
     }
+
+# Create a fixture to get an auth token, used to test
+@pytest.fixture
+def auth_token(client, payload):
     client.post('/user/register', json=payload)  # Register the test user
     response = client.post('/user/login', json={"email": "test@example.com", "password": "password"})
     return response.json['access_token']
 
 # Test register endpoint when everything it's correct
-def test_register_correct(client):
-    payload = {
-        "email": "test@example.com",
-        "password": "password123",
-        "name": "Test",
-        "surname": "User",
-        "location_id": 1
-    }
+def test_register_correct(client, payload):
     response = client.post('/user/register', json=payload)
     assert response.status_code == 201
 
 # Test register endpoint when there's alredy an user registered with that email in the database
-def test_register_alredy_exists(client):
-    payload = {
-        "email": "test@example.com",
-        "password": "password123",
-        "name": "Test",
-        "surname": "User",
-        "location_id": 1
-    }
-    response = client.post('/user/register', json=payload)
-    payload = {
-        "email": "test@example.com",
-        "password": "password123",
-        "name": "Test",
-        "surname": "User",
-        "location_id": 1
-    }
+def test_register_alredy_exists(client, payload):
+    client.post('/user/register', json=payload)
     response = client.post('/user/register', json=payload)
     assert response.status_code == 409
 
 # Test register endpoint when there are fields with incorrect format in the POST request
-def test_register_failed_wrong_format(client):
-    payload = {
-        "email": "test@example",
-        "password": "password123",
-        "name": "Test",
-        "surname": "User",
-        "location_id": 1
-    }
+def test_register_failed_wrong_format(client, payload):
     response = client.post('/user/register', json=payload)
 
     assert response.status_code == 422
@@ -199,6 +187,63 @@ def test_profile_failed_integrity_error(client, auth_token):
     headers = {'Authorization': f'Bearer {auth_token}'}
     response = client.put('/user/profile', json=payload, headers=headers)
 
-    print(response.json)
-
     assert response.status_code == 400
+
+# Test profile endpoint when the user token in the PUT request it's expired
+def test_profile_failed_token_expired(client, expired_token):
+    payload = {
+        "name": "Test",
+        "surname": "User",
+        "password": "test",
+        "location_id": 1
+    }
+
+    headers = {'Authorization': f'Bearer {expired_token}'}
+    response = client.put('/user/profile', json=payload, headers=headers)
+    print(response.json)
+    assert response.status_code == 401
+    assert response.json['msg'] == "Token has expired"
+
+# Test profile endpoint when the user token in the PUT has a wrong format
+def test_profile_failed_token_wrong_format(client, auth_token):
+    payload = {
+        "name": "Test",
+        "surname": "User",
+        "password": "test",
+        "location_id": 1
+    }
+
+    headers = {'Authorization': f'Bearer {auth_token[2:]}'}
+    response = client.put('/user/profile', json=payload, headers=headers)
+    print(response.json)
+    assert response.status_code == 422
+    assert response.json['msg'] == "Invalid header string: 'utf-8' codec can't decode byte 0xc6 in position 2: invalid continuation byte"
+
+# Test profile endpoint when the user token in the PUT request it's invalid
+def test_profile_failed_invalid_token(client, auth_token):
+    payload = {
+        "name": "Test",
+        "surname": "User",
+        "password": "test",
+        "location_id": 1
+    }
+
+    headers = {'Authorization': f'Bearer a{auth_token[1:]}'}
+    response = client.put('/user/profile', json=payload, headers=headers)
+    print(response.json)
+    assert response.status_code == 422
+    assert response.json['msg'] == "Invalid header string: Expecting value: line 1 column 1 (char 0)"
+
+# Test profile endpoint when everything it's correct
+def test_profile_correct(client, auth_token):
+    payload = {
+        "name": "Test",
+        "surname": "User",
+        "password": "test",
+        "location_id": 1
+    }
+
+    headers = {'Authorization': f'Bearer {auth_token}'}
+    response = client.put('/user/profile', json=payload, headers=headers)
+    assert response.status_code == 201
+    assert response.json['message'] == "User profile edited successfully."

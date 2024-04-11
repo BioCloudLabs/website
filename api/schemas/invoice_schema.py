3357017b0bcda5e60815e37e02ddbad5c6dcdd2e
Christian González <chrisgonzaco@gmail.com>
2024-04-11 21:27:23 +0200

    changed invoice schema

--- api/schemas/invoice_schema.py
+++ api/schemas/invoice_schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields, validate
 
 class InvoiceSchema(Schema):
-    price = fields.Float(required=True)
+    price = fields.Float(dump_only=True)
     price_id = fields.Str(required=True)
-    status = fields.Str(required=True, validate=validate.OneOf(["failed, completed, pending"]))
-    user_id = fields.Integer(required=True)
+    status = fields.Str(dump_only=True, validate=validate.OneOf(["failed, completed, pending"]))
+    user_id = fields.Integer(dump_only=True)

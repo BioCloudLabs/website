dad734f6754eac9b28bbf9b6892b5781cafd6668
YourName <aymaneelhanbali@gmail.com>
2024-05-09 16:38:56 +0200

    Started working on the dashboard branch

--- client/src/components/profile/ProfilePage.tsx
+++ client/src/components/profile/ProfilePage.tsx
@@ -85,7 +85,7 @@ const ProfilePage = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md">
-      <h1 className="text-3xl font-bold text-center pt-4 my-12">Edit Profile</h1>
+        <h1 className="text-3xl font-bold text-center pt-4 my-12">Edit Profile</h1>
         <div className="bg-white p-6 rounded-lg shadow-md">
           <form onSubmit={handleSubmit} className="space-y-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
@@ -104,7 +104,6 @@ const ProfilePage = () => {
                 <option key={location.id} value={location.id}>{location.display_name}</option>
               ))}
             </select>
-
             <label htmlFor="credits" className="block text-sm font-medium text-gray-700">Credits</label>
             <input
               id="credits"
@@ -114,25 +113,16 @@ const ProfilePage = () => {
               readOnly
               className="mt-1 block w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded-md shadow-sm focus:outline-none"
             />
-<button type="submit" disabled={isUpdating} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:shadow-outline">
-  Update Profile
-</button>
-
-
-
-
-
-<button
-  type="button"
-  onClick={() => navigate('/change-password')}
-  className="mt-4 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
->
-  Change Password
-</button>
-
-
+            <button type="submit" disabled={isUpdating} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:shadow-outline">
+              Update Profile
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate('/change-password')}
+              className="mt-4 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              Change Password
+            </button>
           </form>
-
         </div>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         {successMessage && <p className="text-green-500">{successMessage}</p>}

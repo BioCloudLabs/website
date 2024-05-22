e0519e2dc9d48b6dbb554f19619018d4131e5b59
Aymaneeee <aymaneelhanbali@gmail.com>
2024-05-22 20:26:13 +0200

    Added blank target to improve redirecting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -194,7 +194,7 @@ function App() {
 
             <div className="flex justify-center md:justify-center items-center space-x-4">
 
-              <a href="https://github.com/BioCloudLabs/" className="text-black-700 hover:text-black-900  text-black-300  hover:text-white group">
+              <a href="https://github.com/BioCloudLabs/" target='_blank' className="text-black-700 hover:text-black-900  text-black-300  hover:text-white group">
                 <svg className="w-6 h-6 group-hover:fill-black fill-gray-700  fill-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
                 </svg>
@@ -229,7 +229,7 @@ function App() {
         </div>
         <div className="w-full mx-auto max-w-screen-xl p-4 text-sm text-black-700 sm:text-center  text-black-400 mt-2 flex flex-col md:flex-row justify-between items-center space-y-2 md:space-y-0">
           <p className="text-center md:text-left w-full md:w-auto">
-            <a property="dct:title" rel="cc:attributionURL" href="https://github.com/BioCloudLabs/">BioCloudLabs</a> by
+            <a property="dct:title" rel="cc:attributionURL" href="https://github.com/BioCloudLabs/" target='_blank'>BioCloudLabs</a> by
             <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/orgs/BioCloudLabs/people"> BioCloudLabs Team</a> is licensed under
             <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" className="inline-flex items-center">
               &nbsp;CC BY-NC-ND 4.0 {/* Added "&nbsp;" for an extra space */}
